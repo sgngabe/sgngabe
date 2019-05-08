37d595d4eef9da5dbcea7319fc3c30733843bc75
sgngabe <gabriel@jamcity.com>
2019-05-08 11:54:37 -0700

    Add files via upload

---
+++ RGHP_QTE_02/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1557297974,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"bg1_para-sheet0.png",
+		"fs_scoresheet-sheet0.png",
+		"quaffle_goalsheet-sheet0.png",
+		"walls-sheet0.png",
+		"spawner1-sheet0.png",
+		"sprite-sheet0.png",
+		"bludger-sheet0.png",
+		"sprite2-sheet0.png",
+		"goal01-sheet0.png",
+		"sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
